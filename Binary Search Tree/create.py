99ba3d87fc84bf86101ba98295cdb7447b67fc05
Althuaf <althuafalthaf@gmail.com>
2023-05-09 11:11:58 +0530

    Binary Search Tree

---
+++ Binary Search Tree/create.py
@@ -0,0 +1,96 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+
+class BinarySearchTree:
+    def __init__(self):
+        self.root = None
+
+    def insert(self, value):
+        if self.root is None:
+            self.root = Node(value)
+        else:
+            self._insert_recursive(self.root, value)
+
+    def _insert_recursive(self, node, value):
+        if value < node.value:
+            if node.left is None:
+                node.left = Node(value)
+            else:
+                self._insert_recursive(node.left, value)
+        else:
+            if node.right is None:
+                node.right = Node(value)
+            else:
+                self._insert_recursive(node.right, value)
+
+    def search(self, value):
+        return self._search_recursive(self.root, value)
+
+    def _search_recursive(self, node, value):
+        if node is None or node.value == value:
+            return node
+        elif value < node.value:
+            return self._search_recursive(node.left, value)
+        else:
+            return self._search_recursive(node.right, value)
+
+    def delete(self, value):
+        self.root = self._delete_recursive(self.root, value)
+
+    def _delete_recursive(self, node, value):
+        if node is None:
+            return node
+
+        if value < node.value:
+            node.left = self._delete_recursive(node.left, value)
+        elif value > node.value:
+            node.right = self._delete_recursive(node.right, value)
+        else:
+            if node.left is None:
+                temp = node.right
+                node = None
+                return temp
+            elif node.right is None:
+                temp = node.left
+                node = None
+                return temp
+
+            temp = self._find_min(node.right)
+            node.value = temp.value
+            node.right = self._delete_recursive(node.right, temp.value)
+
+        return node
+
+    def _find_min(self, node):
+        current = node
+        while current.left is not None:
+            current = current.left
+        return current
+    
+    
+
+
+# Usage example
+bst = BinarySearchTree()
+bst.insert(5)
+bst.insert(3)
+bst.insert(7)
+bst.insert(1)
+bst.insert(9)
+
+print("Original Binary Search Tree:")
+print("Search:")
+print(bst.search(7))  # Output: <__main__.Node object at 0x...>
+
+print("\nDeletion:")
+bst.delete(3)
+
+print("After Deletion:")
+
+print("Search:")
+print(bst.search(3))  # Output: None
+print(bst.search(7))  # Output: None

6acfb70aef0fcaf2ee89326ae532a3e69bb3da74
Althuaf <althuafalthaf@gmail.com>
2023-05-17 19:45:57 +0530

    Heap Sort

--- Heap/heap_sort.py
+++ Heap/heap_sort.py
@@ -20,7 +20,7 @@ def heap_sort(arr):
     for i in range(n // 2 - 1, -1, -1):
         heapify(arr, n, i)
 
-
+    print('Max heap :', arr)
     for i in range(n - 1, 0, -1):
         arr[i], arr[0] = arr[0], arr[i] 
         heapify(arr, i, 0)
